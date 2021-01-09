281f3910cc716749889ad44315063625b03cfc77
Eero Talus <eerotal@mbnet.fi>
2021-01-09 15:16:30 +0200

    Add SwarmInstaller fixture to tests + test SwarmInstaller.get_all_names()

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -5,35 +5,80 @@ from mock import patch
 
 from certbot.errors import PluginError
 from docker.client import DockerClient
-from docker.models.nodes import NodeCollection
+from docker.models.nodes import Node, NodeCollection
+from docker.models.secrets import Secret, SecretCollection
 
 from certbot_docker_swarm._internal.installer import SwarmInstaller
 
-class NamespaceMock:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
 class NodeCollectionMock:
     @classmethod
-    def get(cls, *args, **kwargs):
-        return NamespaceMock(attrs={
-            "Spec": {
-                "Role": "manager"
-            }
-        })
+    def list(cls):
+        return [
+            Node(attrs={
+                "ID": "abcde",
+                "Spec": {
+                    "Role": "manager"
+                }
+            })
+        ]
 
     @classmethod
-    def get_not_manager(cls, *args, **kwargs):
-        tmp = cls.get()
+    def get(cls, node_id):
+        for n in cls.list():
+            if n.id == node_id:
+                return n
+
+    @classmethod
+    def get_not_manager(cls, node_id):
+        tmp = cls.get(node_id)
         tmp.attrs["Spec"]["Role"] = "node"
         return tmp
+
+class SecretCollectionMock:
+    def list(cls):
+        ret = []
+
+        return [
+            Secret(attrs={
+                'ID': 'a',
+                'Spec': {
+                    'Name': 'exameple.com_cert_v0',
+                    'Labels': {
+                        'certbot.certificate-fingerprint': 'AA:BB',
+                        'certbot.domain': 'example.com',
+                        'certbot.managed': 'true',
+                        'certbot.name': 'cert',
+                        'certbot.version': '0'
+                    }
+                }
+            }),
+            Secret(attrs={
+                'ID': 'b',
+                'Spec': {
+                    'Name': 'exameple.com_chain_v0',
+                    'Labels': {
+                        'certbot.certificate-fingerprint': 'AA:BB',
+                        'certbot.domain': 'example.com',
+                        'certbot.managed': 'true',
+                        'certbot.name': 'chain',
+                        'certbot.version': '0'
+                    }
+                }
+            })
+        ]
+
+    @classmethod
+    def get(cls, secret_id):
+        for s in cls.list():
+            if s.id == secret_id:
+                return s
 
 class DockerClientMock:
     @classmethod
     def info(cls, *args, **kwargs):
         return {
             "Swarm": {
-                "NodeID": "123456789",
+                "NodeID": "abcde",
                 "LocalNodeState": "active",
                 "Cluster": {
                     "Spec": {
@@ -56,7 +101,15 @@ class TestSwarmInstaller:
     def docker_client(self):
         return DockerClient()
 
+    @pytest.fixture
+    @patch.object(NodeCollection, "get", NodeCollectionMock.get)
+    @patch.object(DockerClient, "info", DockerClientMock.info)
+    def installer(self):
+        """Returns an initialized partially mocked SwarmInstaller."""
+        return SwarmInstaller({}, "docker-swarm", docker_client=DockerClient())
+
     @patch.object(NodeCollection, "get", NodeCollectionMock.get_not_manager)
+    @patch.object(DockerClient, "info", DockerClientMock.info)
     def test_init_not_swarm_raises(self, docker_client):
         with pytest.raises(PluginError):
             SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
@@ -67,12 +120,8 @@ class TestSwarmInstaller:
         with pytest.raises(PluginError):
             SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
 
-    @patch.object(NodeCollection, "get", NodeCollectionMock.get)
-    @patch.object(DockerClient, "info", DockerClientMock.info)
-    def test_keep_secrets_limit(self, docker_client):
-        s = SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
-
-        tmp = s.keep_secrets
+    def test_keep_secrets_limit(self, installer):
+        tmp = installer.keep_secrets
         assert tmp == DockerClientMock.info() \
                                       .get("Swarm") \
                                       .get("Cluster") \
@@ -80,20 +129,18 @@ class TestSwarmInstaller:
                                       .get("Orchestration") \
                                       .get("TaskHistoryRetentionLimit")
 
-    def test_prepare(self, docker_client):
+    def test_prepare(self):
         pass
 
-    @patch.object(NodeCollection, "get", NodeCollectionMock.get)
-    @patch.object(DockerClient, "info", DockerClientMock.info)
-    def test_more_info(self, docker_client):
-        s = SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
-        assert type(s.more_info()) is str
+    def test_more_info(self, installer):
+        assert type(installer.more_info()) is str
 
     def test_secret_from_file(self):
         pass
 
-    def test_get_all_names(self):
-        pass
+    @patch.object(SecretCollection, "list", SecretCollectionMock.list)
+    def test_get_all_names(self, installer):
+        assert installer.get_all_names() == set(["example.com"])
 
     def test_deploy_cert(self):
         pass
@@ -110,11 +157,8 @@ class TestSwarmInstaller:
     def test_enhance(self):
         pass
 
-    @patch.object(NodeCollection, "get", NodeCollectionMock.get)
-    @patch.object(DockerClient, "info", DockerClientMock.info)
-    def test_supported_enhancements(self, docker_client):
-        s = SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
-        assert s.supported_enhancements() == []
+    def test_supported_enhancements(self, installer):
+        assert installer.supported_enhancements() == []
 
     def test_save(self):
         pass
