82198b7e6c25fba6c9a90ae246ba79a55b12b16c
Eero Talus <eerotal@mbnet.fi>
2021-01-13 19:37:14 +0200

    Write tests for certificate deployment

--- tests/test_installer.py
+++ tests/test_installer.py
@@ -3,11 +3,17 @@
 import os
 import time
 import pytest
+import tempfile
+import shutil
 
 import mock
 from mock import patch, call, MagicMock, DEFAULT
 
+from certbot.plugins.common import Installer
 from certbot.errors import PluginError
+from certbot._internal.configuration import NamespaceConfig
+from certbot._internal.constants import CLI_DEFAULTS
+
 from docker.client import DockerClient
 from docker.models.nodes import Node, NodeCollection
 from docker.models.secrets import Secret, SecretCollection
@@ -16,6 +22,7 @@ from docker.types.services import SecretReference
 
 from certbot_docker_swarm._internal.installer import SwarmInstaller
 from certbot_docker_swarm._internal.installer import SwarmInstallerUtils
+from certbot_docker_swarm._internal.SecretSpec import SecretSpec
 
 
 class NodeCollectionDefs:
@@ -141,8 +148,8 @@ class ServiceCollectionDefs:
     def list(cls):
         return [
             Service(attrs={
+                "ID": "qwerty",
                 "Spec": {
-                    "ID": "qwerty",
                     "Name": "Test Service",
                     "TaskTemplate": {
                         "ContainerSpec": {
@@ -174,6 +181,12 @@ class ServiceCollectionDefs:
             })
         ]
 
+    @classmethod
+    def get(cls, service_id):
+        for service in cls.list():
+            if service.id == service_id:
+                return service
+
 
 class DockerClientDefs:
     @classmethod
@@ -199,7 +212,35 @@ class DockerClientDefs:
         return tmp
 
 
-class TestSwarmInstaller:
+class TestSwarmInstaller():
+    @pytest.fixture
+    def config(self):
+        # Setup
+        tempdir = tempfile.mkdtemp()
+        config = NamespaceConfig(MagicMock(**CLI_DEFAULTS))
+        config.verb = "run"
+        config.config_dir = os.path.join(tempdir, "config")
+        config.work_dir = os.path.join(tempdir, "work")
+        config.logs_dir = os.path.join(tempdir, "logs")
+        config.backup_dir = os.path.join(tempdir, "backup")
+        config.cert_path = CLI_DEFAULTS['auth_cert_path']
+        config.fullchain_path = CLI_DEFAULTS['auth_chain_path']
+        config.chain_path = CLI_DEFAULTS['auth_chain_path']
+        config.server = "https://example.com"
+
+        # Make sure directories exist.
+        if not os.path.isdir(config.config_dir):
+            os.mkdir(config.config_dir)
+        if not os.path.isdir(config.work_dir):
+            os.mkdir(config.work_dir)
+        if not os.path.isdir(config.logs_dir):
+            os.mkdir(config.logs_dir)
+
+        yield config
+
+        # Teardown
+        shutil.rmtree(tempdir)
+
     @pytest.fixture
     def docker_client(self):
         return DockerClient()
@@ -207,21 +248,25 @@ class TestSwarmInstaller:
     @pytest.fixture
     @patch.object(NodeCollection, "get", NodeCollectionDefs.get)
     @patch.object(DockerClient, "info", DockerClientDefs.info)
-    def installer(self):
+    def installer(self, config):
         """Returns an initialized partially mocked SwarmInstaller."""
-        return SwarmInstaller({}, "docker-swarm", docker_client=DockerClient())
+        return SwarmInstaller(
+            config,
+            "docker-swarm",
+            docker_client=DockerClient()
+        )
 
     @patch.object(NodeCollection, "get", NodeCollectionDefs.get_not_manager)
     @patch.object(DockerClient, "info", DockerClientDefs.info)
-    def test_init_not_swarm_raises(self, docker_client):
+    def test_init_not_swarm_raises(self, docker_client, config):
         with pytest.raises(PluginError):
-            SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
+            SwarmInstaller(config, "docker-swarm", docker_client=docker_client)
 
     @patch.object(NodeCollection, "get", NodeCollectionDefs.get)
     @patch.object(DockerClient, "info", DockerClientDefs.info_not_swarm)
-    def test_init_not_manager_raises(self, docker_client):
+    def test_init_not_manager_raises(self, docker_client, config):
         with pytest.raises(PluginError):
-            SwarmInstaller({}, "docker-swarm", docker_client=docker_client)
+            SwarmInstaller(config, "docker-swarm", docker_client=docker_client)
 
     def test_keep_secrets_limit(self, installer):
         tmp = installer.keep_secrets
@@ -338,102 +383,61 @@ class TestSwarmInstaller:
         cf = ("D7:5C:60:9E:BE:8F:78:67:1D:0E:16:98:80:96:3A:B5:"
               "FF:88:A7:94:19:75:6D:11:A0:3E:1F:33:21:90:54:7F")
 
-        # This is the "keep" argument we expect is passed to
-        # SwarmInstaller.rm_secrets().
-        keep = DockerClientDefs.info() \
-                               .get("Swarm") \
-                               .get("Cluster") \
-                               .get("Spec") \
-                               .get("Orchestration") \
-                               .get("TaskHistoryRetentionLimit")
-
         with patch.multiple(
             SwarmInstaller,
             is_secret_deployed=DEFAULT,
             secret_from_file=DEFAULT,
-            update_services=DEFAULT,
-            rm_secrets=DEFAULT
+            update_secret_refs=DEFAULT
         ) as values:
-            mock_is_deployed = values["is_secret_deployed"]
-            mock_rm = values["rm_secrets"]
-            mock_update = values["update_services"]
-            mock_new = values["secret_from_file"]
+            mock_is_secret_deployed = values["is_secret_deployed"]
+            mock_secret_from_file = values["secret_from_file"]
+            mock_update_secret_refs = values["update_secret_refs"]
 
             # Make sure all Secrets are considered not deployed.
-            mock_is_deployed.return_value = False
+            mock_is_secret_deployed.return_value = False
 
             # Let's just deploy the certificate to all Secrets since
             # the Secret contents don't really matter anyway.
             installer.deploy_cert("1.example.com", cp, cp, cp, cp)
 
             # Assert that new Secrtes were created.
-            mock_new.assert_has_calls([
+            mock_secret_from_file.assert_has_calls([
                 call("1.example.com", "cert", cp, cf),
                 call("1.example.com", "key", cp, cf),
                 call("1.example.com", "chain", cp, cf),
                 call("1.example.com", "fullchain", cp, cf),
             ], any_order=True)
 
-            # Assert that Services were updated.
-            mock_update.assert_called_once()
-            for arg in mock_update.call_args.args:
+            # Assert that the SecretSpec was updated.
+            mock_update_secret_refs.assert_called_once()
+            for arg in mock_update_secret_refs.call_args.args:
                 assert arg is not None
-
-            # Assert that old Secrets were removed.
-            mock_rm.assert_has_calls([
-                call("1.example.com", "cert", keep),
-                call("1.example.com", "key", keep),
-                call("1.example.com", "chain", keep),
-                call("1.example.com", "fullchain", keep)
-            ], any_order=True)
 
     def test_deploy_cert_already_deployed(self, installer):
         # Certificate path.
         cp = os.path.join(os.path.dirname(__file__), "assets", "cert.pem")
 
-        # This is the "keep" argument we expect is passed to
-        # SwarmInstaller.rm_secrets().
-        keep = DockerClientDefs.info() \
-                               .get("Swarm") \
-                               .get("Cluster") \
-                               .get("Spec") \
-                               .get("Orchestration") \
-                               .get("TaskHistoryRetentionLimit")
-
         with patch.multiple(
             SwarmInstaller,
             is_secret_deployed=DEFAULT,
             secret_from_file=DEFAULT,
-            update_services=DEFAULT,
-            rm_secrets=DEFAULT
+            update_secret_refs=DEFAULT
         ) as values:
-            mock_is_deployed = values["is_secret_deployed"]
-            mock_rm = values["rm_secrets"]
-            mock_update = values["update_services"]
-            mock_new = values["secret_from_file"]
+            mock_is_secret_deployed = values["is_secret_deployed"]
+            mock_secret_from_file = values["secret_from_file"]
+            mock_update_secret_refs = values["update_secret_refs"]
 
             # Make sure all Secrets are considered deployed.
-            mock_is_deployed.return_value = True
+            mock_is_secret_deployed.return_value = True
 
             # Let's just deploy the certificate to all Secrets since
             # the Secret contents don't really matter anyway.
             installer.deploy_cert("1.example.com", cp, cp, cp, cp)
 
-            # Make sure no new Secrets are created.
-            mock_new.assert_not_called()
-
-            # Make sure Service updates are still attempted.
-            mock_update.assert_called_once()
-            for arg in mock_update.call_args.args:
+            # Assert that the SecretSpec was not updated.
+            mock_update_secret_refs.assert_called_once()
+            for arg in mock_update_secret_refs.call_args.args:
                 assert arg is None
-
-            # Assert that old Secrets are removed.
-            mock_rm.assert_has_calls([
-                call("1.example.com", "cert", keep),
-                call("1.example.com", "key", keep),
-                call("1.example.com", "chain", keep),
-                call("1.example.com", "fullchain", keep)
-            ], any_order=True)
 
     @patch.object(SecretCollection, "list", SecretCollectionDefs.list)
     def test_get_secrets(self, installer):
@@ -472,32 +476,30 @@ class TestSwarmInstaller:
     @patch.object(SecretCollection, "list", SecretCollectionDefs.list)
     def test_rm_secrets(self, installer):
         removed = set([])
-
-        def record_removed(self, removed=removed):
+        def record_removed(self):
             removed.add(self.id)
 
         with patch.object(Secret, "remove", record_removed):
-            removed.clear()
-            installer.rm_secrets("2.example.com", "cert", 0)
-            assert removed == set(["c", "e"])
+            installer.rm_secrets(0)
+            assert removed == set(["a", "b", "c", "d", "e", "f"])
 
-            removed.clear()
-            installer.rm_secrets("2.example.com", "cert", 1)
-            assert removed == set(["c"])
+            removed = set([])
+            installer.rm_secrets(1)
+            assert removed == set(["c", "d"])
 
-            removed.clear()
-            installer.rm_secrets("2.example.com", "cert", 10)
-            assert removed == set()
+            removed = set([])
+            installer.rm_secrets(5)
+            assert removed == set([])
 
     @patch.object(SecretCollection, "list", SecretCollectionDefs.list)
     def test_rm_secrets_negative_keep(self, installer):
         with patch.object(Secret, "remove"):
             with pytest.raises(PluginError):
-                installer.rm_secrets("2.example.com", "cert", -1)
+                installer.rm_secrets(-1)
 
     @patch.object(ServiceCollection, "list", ServiceCollectionDefs.list)
     @patch.object(SecretCollection, "get", SecretCollectionDefs.get)
-    def test_update_services(self, installer):
+    def test_update_secret_refs(self, installer):
         cert = None
         chain = None
 
@@ -510,18 +512,15 @@ class TestSwarmInstaller:
             elif secret.id == "f":
                 chain = secret
 
-        with patch.object(Service, "update") as mock_update:
-            installer.update_services(cert, chain, None, None)
+        installer.update_secret_refs(cert, chain, None, None)
 
-            mock_update.assert_called_once()
+        updated = set()
+        for secret_id in installer.secret_spec.services.get("qwerty"):
+            updated.add(secret_id)
 
-            # Assert that all the correct Secrets were updated.
-            updated = set()
-            for ref in mock_update.call_args.kwargs.get("secrets"):
-                assert isinstance(ref, SecretReference)
-                updated.add(ref.get("SecretID"))
-
-            assert updated == set(["e", "f"])
+        # The original Secret IDs are "c" and "d". Assert that
+        # Secrets were updated.
+        assert updated == set(["e", "f"])
 
     @patch.object(ServiceCollection, "list", ServiceCollectionDefs.list)
     @patch.object(SecretCollection, "get", SecretCollectionDefs.get)
@@ -538,12 +537,15 @@ class TestSwarmInstaller:
             elif secret.id == "b":
                 chain = secret
 
-        with patch.object(Service, "update") as mock_update:
-            installer.update_services(cert, chain, None, None)
+        installer.update_secret_refs(cert, chain, None, None)
 
-            # Update shouldn't be called because the Secrets don't
-            # renew the service Secrets.
-            mock_update.assert_not_called()
+        updated = set()
+        for secret_id in installer.secret_spec.services.get("qwerty"):
+            updated.add(secret_id)
+
+        # The original Secret IDs are "c" and "d". Assert that Secrets
+        # were *not* updated.
+        assert updated == set(["c", "d"])
 
     @patch.object(SecretCollection, "get", SecretCollectionDefs.get)
     def test_renew_secret_reference(self, installer):
@@ -579,17 +581,66 @@ class TestSwarmInstaller:
     def test_supported_enhancements(self, installer):
         assert installer.supported_enhancements() == []
 
-    @pytest.mark.skip(reason="Nothing to test.")
-    def test_save(self):
-        pass
+    def test_save(self, installer):
+        # This is the "keep" argument we expect is passed to
+        # SwarmInstaller.rm_secrets().
+        keep = DockerClientDefs.info() \
+                               .get("Swarm") \
+                               .get("Cluster") \
+                               .get("Spec") \
+                               .get("Orchestration") \
+                               .get("TaskHistoryRetentionLimit")
 
-    def test_rollback_checkpoints(self, installer):
-        with pytest.raises(PluginError):
-            installer.rollback_checkpoints()
+        with patch.multiple(
+            SwarmInstaller,
+            add_to_checkpoint=DEFAULT,
+            finalize_checkpoint=DEFAULT,
+            rm_secrets=DEFAULT,
+            update_services=DEFAULT
+        ) as values:
+            mock_add_to_checkpoint = values["add_to_checkpoint"]
+            mock_finalize_checkpoint = values["finalize_checkpoint"]
+            mock_rm_secrets = values["rm_secrets"]
+            mock_update_services = values["update_services"]
 
-    @pytest.mark.skip(reason="Nothing to test.")
-    def test_recovery_routine(self):
-        pass
+            installer.save("Test title", False)
+
+            # Assert that the config file was added to a checkpoint.
+            mock_add_to_checkpoint.assert_called_once_with(
+                installer.conffile,
+                mock.ANY,
+                False
+            )
+
+            mock_finalize_checkpoint.assert_called_once_with("Test title")
+            mock_update_services.assert_called_once()
+            mock_rm_secrets.assert_called_once_with(keep)
+
+    @patch.object(ServiceCollection, "get", ServiceCollectionDefs.get)
+    def test_update_services(self, installer, docker_client):
+        # Construct a SecretSpec from the existing test Service.
+        spec = SecretSpec(docker_client)
+        for service in ServiceCollectionDefs.list():
+            refs = service.attrs \
+                          .get("Spec") \
+                          .get("TaskTemplate") \
+                          .get("ContainerSpec") \
+                          .get("Secrets")
+
+            for ref in refs:
+                spec.add_ref(service.id, ref)
+
+        installer.secret_spec = spec
+
+        with patch.object(Service, "update") as mock_update:
+            installer.update_services()
+
+            calls = []
+            for service_id in spec.services:
+                calls.append(call(secrets=spec.get_refs(service_id)))
+
+            mock_update.assert_has_calls(calls)
+
 
     @pytest.mark.skip(reason="Nothing to test.")
     def test_config_test(self):
